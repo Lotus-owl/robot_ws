8fb36e9ca11f00994f9f6fa5f4127c5affa66a43
윤소연 <yunsy616@gmail.com>
2024-08-23 16:03:19 +0900

    Add files via upload

---
+++ src/move_t_sim/src/moveTurtleBot.cpp
@@ -0,0 +1,10 @@
+#include "move_t_sim/moveTBot.hpp"
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<MoveTurtleBot>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
